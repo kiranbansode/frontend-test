1f2ced684d41714c307a909b751847feb6397245
Kiran Bansode <kiran5120135@gmail.com>
2023-06-09 16:56:11 +0530

    Solve Coding Challenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [clientIpAdd, setClientIpAdd] = useState(null);
+
+	useEffect(() => {
+		let setTimeoutId;
+
+		(async () => {
+			/* 
+        Note:-
+        This is free service, So sometime network request can be slow.
+        If you unable to see ip address, then it is highly chance that network request is still pending.
+        You can check request's status in Developer Tool's `Network tab` [Choose `Fetch/XHR`]. 
+        Sometimes mine took almost a minute to get client's ip address
+      */
+			await fetch("https://api.ipify.org?format=json")
+				.then((res) => res.json())
+				.then((data) => setClientIpAdd(data.ip))
+				// Usually if we have an error we can either use `Modal` or `Alert` component.
+				// ErrorBoundary also can be used.
+				.catch((err) => err);
+		})();
+
+		// This will clear out setTimeout() using it's id when App component get's unmounted
+		return () => {
+			clearTimeout(setTimeoutId);
+		};
+	}, []);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				{clientIpAdd ? (
+					<p>
+						Client IP:
+						<span className="client-ip">{clientIpAdd}</span>
+					</p>
+				) : (
+					<p className="loading">Loading...</p>
+				)}
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Checking <App/> component", () => {
+	test("renders React Logo", () => {
+		render(<App />);
+		const linkElement = screen.getByAltText(/logo/i);
+		expect(linkElement).toBeInTheDocument();
+	});
+
+	/* 
+		If this test broke, just refresh all tests. 
+	*/
+	test("renders correct ip address", async () => {
+		render(<App />);
+
+		const ip = await fetch("https://api.ipify.org?format=json")
+			.then((res) => res.json())
+			.then((data) => data.ip);
+
+		const clientIp = await screen.findByText(
+			/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/i
+		);
+
+		expect(clientIp.textContent).toEqual(ip);
+	}, 60000); // setting timeout to 1 minute. Sometimes `ipify` api are slow
+
+	test("renders 'Loading...' text", () => {
+		render(<App />);
+		const loadingPara = screen.queryByText("Loading...");
+		expect(loadingPara).toBeInTheDocument();
+	});
 });
